63765cefad5ba44e831bfeebedc47ce1105d72f7
pasapicella <pas.apicella@tpg.com.au>
2015-10-14 14:49:29 +1100

    New Files added and got Login working

---
+++ src/main/java/com/ibm/pas/bluemix/pgweb/controller/HomeController.java
@@ -0,0 +1,52 @@
+package com.ibm.pas.bluemix.pgweb.controller;
+
+import com.ibm.pas.bluemix.pgweb.utils.AdminUtil;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.sql.Connection;
+
+@Controller
+public class HomeController
+{
+    protected static Logger logger = Logger.getLogger("controller");
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String login(Model model, HttpServletResponse response, HttpServletRequest request, HttpSession session) throws Exception
+    {
+        logger.debug("Received request to show home page");
+
+        if (session.getAttribute("user_key") == null)
+        {
+            logger.debug("user_key is null new Login required");
+            response.sendRedirect(request.getContextPath() + "/login");
+            return null;
+        }
+        else
+        {
+            Connection conn = AdminUtil.getConnection((String) session.getAttribute("user_key"));
+            if (conn == null )
+            {
+                response.sendRedirect(request.getContextPath() + "/login");
+                return null;
+            }
+            else
+            {
+                if (conn.isClosed())
+                {
+                    response.sendRedirect(request.getContextPath() + "/login");
+                    return null;
+                }
+            }
+
+        }
+
+        return "main";
+    }
+}
