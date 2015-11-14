a22feb2c7e7e522fbaa78c151b893c98514d61c7
pasapicella <pas.apicella@tpg.com.au>
2015-11-14 15:43:32 +1100

    Added Code to auto login if ElephantSQL service is bound to application

--- src/main/java/com/ibm/pas/bluemix/pgweb/controller/LoginController.java
+++ src/main/java/com/ibm/pas/bluemix/pgweb/controller/LoginController.java
@@ -7,6 +7,8 @@ import com.ibm.pas.bluemix.pgweb.utils.ConnectionManager;
 import com.ibm.pas.bluemix.pgweb.utils.PostgresConnection;
 import com.ibm.pas.bluemix.pgweb.utils.QueryUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.stereotype.Controller;
 import org.apache.log4j.Logger;
 import org.springframework.ui.Model;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpSession;
 import java.sql.Connection;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -32,15 +35,64 @@ public class LoginController
     {
         logger.info("Received request to show login page");
 
-        String vcapServices = null;
-        vcapServices = System.getenv().get("VCAP_SERVICES");
+        String jsonString = null;
+        jsonString = System.getenv().get("VCAP_SERVICES");
 
-        if (vcapServices != null)
+        if (jsonString != null)
         {
-            if (vcapServices.length() > 0)
+            if (jsonString.length() > 0)
             {
-                // check whether PostgreSQL service exists
-                // FIRST = ElephantSQL service
+                try
+                {
+                    ConnectionManager cm = ConnectionManager.getInstance();
+                    Connection conn;
+
+                    // check whether PostgreSQL service exists
+                    // FIRST = ElephantSQL service
+
+                    logger.info("** Attempting login using VCAP_SERVICES **");
+                    logger.info(jsonString);
+
+                    Login login = parseLoginCredentials(jsonString);
+
+                    conn = AdminUtil.getNewConnection
+                            (login.getUrl(), login.getUsername(), login.getPassword());
+
+                    conn.setAutoCommit(true);
+
+                    PostgresConnection newConn =
+                            new PostgresConnection
+                                    (conn, login.getUrl(), new java.util.Date().toString(), login.getUsername().toUpperCase());
+
+                    cm.addConnection(newConn, session.getId());
+
+                    session.setAttribute("user_key", session.getId());
+                    session.setAttribute("user", login.getUsername().toUpperCase());
+                    session.setAttribute("schema", "public");
+                    session.setAttribute("url", login.getUrl());
+                    session.setAttribute("prefs", userPref);
+                    session.setAttribute("history", new LinkedList());
+                    session.setAttribute("connectedAt", new java.util.Date().toString());
+
+                    Map<String, String> schemaMap = AdminUtil.getSchemaMap();
+
+                    schemaMap = QueryUtil.populateSchemaMap
+                            (conn, schemaMap, "public");
+
+                    session.setAttribute("schemaMap", schemaMap);
+
+                    logger.info("schemaMap=" + schemaMap);
+                    logger.info(userPref.toString());
+
+                    return "main";
+
+                }
+                catch (Exception ex)
+                {
+                    // we tried if we can't auto login , just present login screen
+                    model.addAttribute("loginObj", new Login("", "", "jdbc:postgresql://localhost:5432/apples"));
+                    logger.info("Auto Login via VCAP_SERVICES Failed - " + ex.getMessage());
+                }
 
             }
         }
@@ -111,5 +163,41 @@ public class LoginController
             model.addAttribute("loginObj");
             return "login";
         }
+    }
+
+    private Login parseLoginCredentials (String jsonString)
+    {
+        Login login = new Login();
+        JsonParser parser = JsonParserFactory.getJsonParser();
+
+        Map<String, Object> jsonMap = parser.parseMap(jsonString);
+
+        List clearDBList = (List) jsonMap.get("elephantsql");
+        Map clearDBMap = (Map) clearDBList.get(0);
+        Map credentailsMap = (Map) clearDBMap.get("credentials");
+
+        String uri = (String) credentailsMap.get("uri");
+        String url = null, username = null, password = null;
+
+        int firstcolon = uri.indexOf(':');
+        int firstatsymbol = uri.indexOf('@');
+
+        String subStr = uri.substring(11);
+        String subStr2 = uri.substring(firstatsymbol + 1);
+
+        username = subStr.substring(0, subStr.indexOf(':'));
+        password = subStr.substring(subStr.indexOf(':') + 1, subStr.indexOf('@'));
+
+        //System.out.println(subStr);
+        //System.out.println(subStr2);
+
+        url = "jdbc:postgresql://" + subStr2.substring(0, subStr2.indexOf(':'));
+
+        login.setUsername(username);
+        login.setPassword(password);
+        login.setUrl(url);
+
+        return login;
+
     }
 }
